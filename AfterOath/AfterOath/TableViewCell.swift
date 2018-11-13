8b5add691780a0897e7943264c4cd76227991199
Mike Laursen <mailbox@laursen.org>
2018-11-13 10:57:26 -0800

    Set up basic project.

---
+++ AfterOath/AfterOath/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  AfterOath
+//
+//  Created by Mike Laursen on 11/13/18.
+//  Copyright © 2018 Appamajigger. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
