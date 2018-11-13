7ea3ca96da7521f6ef89b1d0f8f8130b17b24968
Mike Laursen <mailbox@laursen.org>
2018-11-13 11:14:45 -0800

    Laid out thumbnail table cell.

--- AfterOath/AfterOath/TableViewController.swift
+++ AfterOath/AfterOath/TableViewController.swift
@@ -24,23 +24,20 @@ class TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        // #warning Number of rows temporarily set to 1
+        return 1
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "thumbnailCell", for: indexPath) as! TableViewCell
         // Configure the cell...
-
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
